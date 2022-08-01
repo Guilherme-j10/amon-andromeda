db1292be54a24082c209933d2c4cfb4f7523e8dd
Guilherme Campos <guilhermeZA.123123@gmail.com>
2022-08-01 16:23:29 -0300

    export types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,2 @@
 export { Andromeda } from './package/andromeda';
+export { AndromedaProps, AndromedaStorageConnection, IAndromeda, IBaileys, IExistenceOnWhatsApp, IListMessageDefinitions, ITypeDeviceWithMessage, MessagesType } from './package/Dtos/interface';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 export { Andromeda } from './package/andromeda';
+export { 
+  AndromedaProps, 
+  AndromedaStorageConnection, 
+  IAndromeda, 
+  IBaileys, 
+  IExistenceOnWhatsApp, 
+  IListMessageDefinitions, 
+  ITypeDeviceWithMessage, 
+  MessagesType 
+} from './package/Dtos/interface';
