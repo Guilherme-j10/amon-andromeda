4e57a1746caab6e971e9181f6f7ea35fcda72ad5
Guilherme Campos <guilhermeZA.123123@gmail.com>
2022-09-01 12:59:26 -0300

    update baileys version

--- src/package/andromeda.ts
+++ src/package/andromeda.ts
@@ -135,6 +135,7 @@ export const Andromeda = async (initializerProps: AndromedaProps): Promise<IAndr
         if(shouldReconnect) {
   
           const client = await Andromeda(initializerProps);
+          IS_CONNECTED = true;
           resolve(client);
 
           return;
